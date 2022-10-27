ee2d6944122f8804e7d6f48bd7c350b754e8f32f
Shane Dorsey <scdorsey22@gmail.com>
2022-10-27 15:10:11 -0700

    Basic setup complete

--- phone-backend/index.js
+++ phone-backend/index.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors= require('cors');
+const cors = require('cors');
 
-const phonesRouter = require('./controllers/phone');
+const decodeIDToken = require('./authenticateToken');
+const phonesRouter = require('./controllers/phones');
+const app = express();
 
 mongoose.connect(
-    'mongodb+srv://colonel:colonel123@cluster0.ptn5m3o.mongodb.net/?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log('Connected to database');
-    })
-    .catch((err) => {
-      console.log('Error connecting to DB', err.message);
-    });
-
-const app = express();
+    'mongodb+srv://colonel:colonel123@cluster0.ptn5m3o.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((err) => {
+    console.log('Error connecting to DB', err.message);
+  });
 app.use(cors());
+app.use(express.json());
+app.use(decodeIDToken);
 
-app.use('/api', phonesRouter)
+app.use('/api', phonesRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World');
--- phone-frontend/src/components/phonebook/ListAllNumbers.jsx
+++ phone-frontend/src/components/phonebook/ListAllNumbers.jsx
@@ -1,27 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getPhonebookEntries } from '../../services/phonebookServices';
 
 const ListAllNumbers = () => {
-    return (
-        <div>
-            <Link to='add-number'>Add Number</Link>
-            <h2>Phone Numbers</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Number</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Foo Bar</td>
-                        <td>(999) 888-7777</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  const [entries, setEntries] = useState();
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      const fetchedEntries = await getPhonebookEntries();
+      setEntries(fetchedEntries);
+    }
+    fetchEntries();
+  }, [])
+
+  if (entries === undefined) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Link to="/add-number">Add number</Link>
+      <h2>Phone numbers</h2>
+      
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry) => (
+          <tr>
+            <td>{entry.name}</td>
+            <td>{entry.number}</td>
+            <button>Edit</button>
+            <button>Delete</button>
+          </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  ) 
+};
 
 export default ListAllNumbers;
